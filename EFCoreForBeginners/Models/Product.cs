bedf1be0a904bf182e0ee918cee7909913d605cb
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-01-11 13:58:34 +0300

    Entity Framework Core for Beginners: Getting Started

---
+++ EFCoreForBeginners/Models/Product.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreForBeginners.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    [Column(TypeName = "decimal(6, 2)")]
+    public decimal Price { get; set; }
+}
---
+++ EFCoreForBeginners/Program.cs
@@ -0,0 +1,36 @@
+﻿using EFCoreForBeginners.Data;
+using EFCoreForBeginners.Models;
+
+using PizzaContext context = new();
+
+var veggieSpecial = new Product()
+{
+    Name = "Veggie Special",
+    Price = 9.99M,
+};
+context.Products.Add(veggieSpecial);
+
+var deluxeMeat = new Product()
+{
+    Name = "Deluxe Meat",
+    Price = 12.99M,
+};
+context.Products.Add(deluxeMeat);
+
+context.SaveChanges();
+
+veggieSpecial.Name = "Veggie Special (Updated)";
+veggieSpecial.Price = 10.99M;
+context.SaveChanges();
+
+var products = context.Products
+    .Where(p => p.Price > 10.00M)
+    .OrderBy(p => p.Name);
+
+foreach (var product in products)
+{
+    Console.WriteLine($"Id:    {product.Id}");
+    Console.WriteLine($"Name:  {product.Name}");
+    Console.WriteLine($"Price: {product.Price}");
+    Console.WriteLine(new string('-', 20));
+}
